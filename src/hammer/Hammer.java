cb97002ce4d1fd12d5dc11ce990e94ca5fdaace1
Brian Stephanik <xoedusk@gmail.com>
2012-12-16 22:12:08 -0800

    Initial commit of important files.

---
+++ src/hammer/Hammer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hammer;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ *
+ *
+ */
+public class Hammer extends SimpleApplication{
+
+    @Override
+    public void simpleInitApp() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
