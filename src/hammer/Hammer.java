08e7489aad194f08b918981468f569af25c440de
Brian Stephanik <xoedusk@gmail.com>
2012-12-16 23:10:24 -0800

    Initial commit of many of the required JME3 files.

--- src/hammer/Hammer.java
+++ src/hammer/Hammer.java
@@ -1,17 +1,39 @@
 package hammer;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
 /**
  * This is the primary class for the game. Its constructor is called
  * when the game opens.
  *
  */
-public class Hammer extends SimpleApplication{
+public class Hammer extends SimpleApplication {
+    
+    public static void main(String[] args) {
+        Hammer app = new Hammer();
+        app.start();
+    }
 
     @Override
     public void simpleInitApp() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
     }
     
 }
